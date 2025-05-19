4d41455a4d57c60ef0376e9a3def8c3f877602db
Hemesh-P <hparshotam11@gmail.com>
2025-05-19 16:52:37 +0200

    Splash screen

---
+++ app/src/main/java/com/example/mainapplication/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.mainapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long SPLASH_SCREEN_DURATION = 2000; // 2 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start the main activity
+                Intent mainIntent = new Intent(SplashActivity.this, SignInActivity.class);
+                startActivity(mainIntent);
+                finish(); // Close the splash activity
+            }
+        }, SPLASH_SCREEN_DURATION);
+    }
+}
